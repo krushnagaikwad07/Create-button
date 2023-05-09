ba0818c0dc6522665e29b20008e861bf72c08438
krushnagaikwad07 <krushnagaikwad7799@gmail.com>
2023-05-10 01:57:29 +0530

    Custom-Buttom

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import './Button.css'
+import Button from './Button';
+function App(){
+ return(
+  <div className="container">
+   <h1>React Button Component</h1>
+   <div className="inner-container">
+     <Button/>
+   </div>
+  </div>
+ )
+}
+export default App;
---
+++ src/Button.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+	<div className='main-container'>
+	    <div className="button-container">
+      <button className="custom-button blue">Click me</button>
+     </div>
+     <div className="button-container">
+      <button className="custom-button green">Click me</button>
+     </div>
+     <div className="button-container">
+      <button className="custom-button red">Click me</button>
+     </div>
+	</div>
+  )
+}
+
+export default Button
